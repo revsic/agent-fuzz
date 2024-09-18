4af9b17ebf13992208273442acdfcdb53d98f5ef
revsic <revsic99@gmail.com>
2024-09-18 17:29:24 +0900

    build: Add initial cjson benchmark build script

---
+++ benchmark/cjson/build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+PROJECT="cjson"
+# | workspace
+#   | src
+#     | cJSON (repo)
+#   | build
+#   | lib
+#     | libcjson.a
+#   | include
+#     | cJSON.h
+#   | corpus
+#   | dict
+#     | json.dict
+
+WORKDIR=./$PROJECT/workspace
+mkdir -p $WORKDIR
+# to absolute path
+WORKDIR=`realpath $WORKDIR`
+pushd $WORKDIR
+
+SRCDIR=$WORKDIR/src
+mkdir -p $SRCDIR && pushd $SRCDIR
+# clone the project
+git clone https://github.com/DaveGamble/cJSON
+# specify the version
+pushd cJSON
+git checkout v1.7.18
+
+BUILDIR=$WORKDIR/build
+mkdir -p $BUILDIR && pushd $BUILDIR
+# build
+cmake -DBUILD_SHARED_AND_STATIC_LIBS=ON -DENABLE_CJSON_TEST=OFF $SRCDIR/cJSON
+make -j 4
+
+mkdir -p $WORKDIR/lib
+cp libcjson.a $WORKDIR/lib
+# $SRCDIR/cJSON
+popd
+
+pwd
+
+# copy header files
+mkdir -p $WORKDIR/include
+cp cJSON.h $WORKDIR/include
+
+# copy corpus
+mkdir -p $WORKDIR/corpus
+cp fuzzing/inputs/* $WORKDIR/corpus
+
+# copy dict
+mkdir -p $WORKDIR/dict
+cp fuzzing/json.dict $WORKDIR/dict
+
+# $SRCDIR
+popd
+# $WORKDIR
+popd
+
+popd
